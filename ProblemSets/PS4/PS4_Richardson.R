7d00bdaf7b3d2801fa5bc5473df072a9e038d351
Travis Richardson <twrichardson@ou.edu>
2019-02-13 10:12:43 -0600

    Completed PS4

---
+++ ProblemSets/PS4/PS4_Richardson.R
@@ -0,0 +1,5 @@
+system("wget -O nflstats.json 'http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json'")
+system("cat 'nflstats.json'")
+library(jsonlite)
+mydf<-fromJSON('nflstats.json')
+class(mydf$player)
